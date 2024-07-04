b27403f65a3fc630a407d0b09f6c3937043f687e
Benno <81779508+Westpol@users.noreply.github.com>
2024-07-04 13:43:37 +0200

    Update testing.py

--- oldfiles/testing.py
+++ oldfiles/testing.py
@@ -1,7 +1,13 @@
 import requests
+import os
+import time
+import sys
 
+time.sleep(5)
 url = "https://api.github.com/repos/Westpol/GeS-Ipad-Verwaltung/releases/latest"
 
 response = requests.get(url)
 
 print(response.json()["name"])
+
+os.execv(sys.argv[0], sys.argv)
